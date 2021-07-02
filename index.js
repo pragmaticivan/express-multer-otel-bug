a3db3789b6a8867cc5e5288a73979593bb635aa2
Ivan Santos <pragmaticivan@gmail.com>
2021-07-01 19:32:43 -0500

    chore: uncomment multer

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const {trace, context} = require('@opentelemetry/api');
 
 
-// Uncomment Multer to reproducer error
-// var multer  = require('multer')
-// var upload = multer({ dest: 'uploads/' })
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 const PORT = process.env.PORT || "8080";
 const app = express();
+
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+})
+
 
 app.get("/", (req, res) => {
   const span = trace.getSpan(context.active());
@@ -15,11 +20,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// Uncomment multer usage to reproduce error
-// app.post('/profile', upload.single('avatar'), function (req, res, next) {
-//   // req.file is the `avatar` file
-//   // req.body will hold the text fields, if there were any
-// })
 
 app.listen(parseInt(PORT, 10), () => {
   console.log(`Listening for requests on http://localhost:${PORT}`);
